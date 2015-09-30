fea4a90983668c7528cb146204a09f6ab3d2e84d
ansilva <anderson.nuneseth@gmail.com>
2015-09-30 14:49:57 -0300

    Add initial data

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ You should include the jQuery library on your project
 
 ```html
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script type="text/javascript" src="replicant.jquery.min.js"></script>
+    <script type="text/javascript" src="replicant.jquery.min2.js"></script>
 ```
 
 After that, you must create your HTML as the following:
@@ -21,5 +21,27 @@ After that, you must create your HTML as the following:
     </div>
     <script type="text/javascript">
         $("#my-replicant").replicant();
+    </script>
+```
+
+#Replicant with initial data
+
+```html
+    <div id="my-replicant">
+        <div class="replicant-base">
+            Email: <input type="email" name="emailAdrress[]"><br>
+            Full Name: <input type="text" name="fullName[]">
+            <a href="javascript: void(0)" class="replicant-add">Add Another Email</a>
+            <a href="javascript: void(0)" class="replicant-remove">Remove</a>
+        </div>
+    </div>
+    <script type="text/javascript">
+        $("#my-replicant").replicant({
+            values : [
+                {'emailAddress[]' : 'myfirstemail@gmail.com', 'fullName[]' : 'Anderson Nunes'},
+                {'emailAddress[]' : 'mysecondemail@gmail.com', 'fullName[]' : 'Celia Nunes'},
+                {'emailAddress[]' : 'mythird@gmail.com', 'fullName[]' : 'Calebe Nunes'}
+            ]
+        });
     </script>
 ```
--- replicant.jquery.js
+++ replicant.jquery.js
@@ -1,6 +1,7 @@
-$.fn.replicant = function(){
+$.fn.replicant = function(initialData){
     var model = $(this);
     model = $(model);
+    var self = this;
     this.totalItems = 0;
     this.model = $(".replicant-base", model).prop('outerHTML');
     this.container = model;
@@ -11,16 +12,37 @@ $.fn.replicant = function(){
 
     //action for add button
     model.delegate(".replicant-add", "click", function(){
-        console.log('base: ',this.model);
-        this.container.append(this.model);
-        //when I replicate a new object, all remove buttons must be showed (except the last one):
-        $(".replicant-remove", this.container).show();
-        $(".replicant-remove:last", this.container).hide();
-        $(".replicant-add", this.container).not(':last').hide();
+        self.replicate();
     });
 
     //action for remove button
     model.delegate(".replicant-remove", "click", function(){
         $(this).closest(".replicant-base").remove();
     });
+
+    this.replicate = function(value, ignoreAppend){
+        console.log('base: ',this.model);
+        if(!ignoreAppend) {
+            this.container.append(this.model);
+        }
+        //when I replicate a new object, all remove buttons must be showed (except the last one):
+        $(".replicant-remove", this.container).show();
+        $(".replicant-remove:last", this.container).hide();
+        $(".replicant-add", this.container).not(':last').hide();
+
+        //setting data
+        if(value){
+            for(var field in value){
+                $('[name="' + field +'"]', $(".replicant-base:last", this.container)).attr('value', value[field]);
+                console.log('campo: ', field, value[field], $('[name="' + field +'"]', $(".replicant-base", this.container)), $('[name="' + field +'"]', $(".replicant-base", this.container)).length);
+            }
+        }
+    };
+
+    //Do I need to put initial data here?
+    if(initialData && initialData.values){
+        for(var x = 0; x < initialData.values.length; x++) {
+            this.replicate(initialData.values[x], (x == 0));
+        }
+    }
 }
